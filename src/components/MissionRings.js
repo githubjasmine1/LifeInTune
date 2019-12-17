e4b05b285637b6d1c8548d03a358285dc292e07a
Maciej Leszczyński <dev@asista.pl>
2019-12-18 00:43:05 +0100

    Work

--- src/components/MissionRings.js
+++ src/components/MissionRings.js
@@ -1,5 +1,5 @@
 import React, { memo, useEffect, useRef, useState } from 'react'
-import { Power1, Power2, Power3, Power4 } from 'gsap'
+import { Power1, Power2, Power3 } from 'gsap'
 
 import { useControlledTimeline } from '../hooks/animation'
 import useInView from '../hooks/useInView'
@@ -49,7 +49,7 @@ const MissionRings = memo(({ className, ...props }) => {
         rotation: -90,
         strokeWidth: 0,
         drawSVG: '0% 0%',
-        ease: Power4.easeInOut,
+        ease: Power3.easeInOut,
       },
       0.5,
       0
